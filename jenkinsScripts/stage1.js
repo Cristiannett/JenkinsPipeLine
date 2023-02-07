b62ed51be1f45bab544d2986325f68eb387f34e0
cristian <cristiandk49@gmail.com>
2023-02-07 19:48:12 +0100

    stages_condicionales_v1

--- jenkinsScripts/stage1.js
+++ jenkinsScripts/stage1.js
@@ -1,6 +1,6 @@
 const script1Param = process.argv[2];
 
-if (script1Param === "Leonardo") {
+if (script1Param === "leonardo") {
     console.log("success");
     process.exit(0);
 } else {
--- jenkinsScripts/stage2.js
+++ jenkinsScripts/stage2.js
@@ -1,6 +1,6 @@
 const script2Param = process.argv[2];
 
-if (script2Param === "pero") {
+if (script2Param === "davinci") {
     console.log("success");
     process.exit(0);
 } else {
